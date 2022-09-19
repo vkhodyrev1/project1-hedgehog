ce61a4f00bff30238f24bcdd9ca6bd82ab02fe45
VKhodyrev <vkhodyrev1@gmail.com>
2022-09-19 15:41:29 +0300

    solved sizing problem

--- js/main.js
+++ js/main.js
@@ -3,22 +3,57 @@ class Game {
         this.hadgehog = null; //will store an instance of the class Player
         this.apples = []; //will store instances of the class Obstacle
         this.applesDropped = [];
+        this.obstacles = [];
     }
     start() {
-        this.hadgehog = new Hadgehog (15, 15, 50, 0, "hadgehog", "board");
+        
+
+        //let hadgehogWidth = 15;
+        //let hadgehogHeigth = 15;
+//
+        //let appleWidth = 8;
+        //let appleHeigth = 8;
+//
+        //let obstacleWidth = 15;
+        //let obstacleHeigth = 10;
+        //let sizes = [[15, 8, 15], [15, 8, 10]];
+        let sizes = [
+            {
+                width: 15,
+                heigth: 15
+            },
+            {
+                width: 8,
+                heigth: 8
+            },
+            {
+                width: 15,
+                heigth: 10
+            }
+        ]
+        if (window.innerWidth >= window.innerHeight) {
+            sizes.forEach((widthItem) => {
+                widthItem.width = widthItem.width*window.innerHeight/window.innerWidth;
+                //console.log(widthItem.width);
+                //console.log(sizes);
+            })
+        } else {
+            sizes.forEach((heightItem) => {
+                heightItem.heigth = heightItem.heigth*window.innerWidth/window.innerHeight;
+            })
+        }
+        //console.log(sizes);
+        this.hadgehog = new Hadgehog (sizes[0].heigth, sizes[0].width, 50, 0, "hadgehog", "board", "url('/project1-hedgehog/images/hadgehogHome.png");
         this.attachEventListeners();
-        
-        let appleWidth = 8;
-        let appleHeigth = appleWidth;
-        
+
         if (this.applesDropped.length === 0) {
             //console.log(this.applesDropped);
             const applesAppear = setInterval(() => {
                 
-                let randomAppleX = Math.floor(Math.random()*(51 - appleWidth) + 50);
+                let randomAppleX = Math.floor(Math.random()*(51 - sizes[1].width) + 50);
                 let limitY = 60 - (randomAppleX - 75)**2/10;
-                let randomAppleY = Math.floor(Math.random()*(limitY - appleHeigth) + 40);
-                const appleInstance = new Apple(appleHeigth, appleWidth, randomAppleX, randomAppleY, "apple", "board");
+                let randomAppleY = Math.floor(Math.random()*(limitY - sizes[1].heigth) + 40);
+                const appleInstance = new Apple(sizes[1].heigth, sizes[1].width, randomAppleX, randomAppleY, "apple", "board", "url('/project1-hedgehog/images/apple.png')");
                 this.apples.push(appleInstance);
                 if (this.apples.length >10) {
                     clearInterval(applesAppear);
@@ -26,14 +61,43 @@ class Game {
                 
             }, 10);
         }
-        
-    }
 
+        
+        const obstaclesAppear = setInterval(() => {
+            let randomObstacleX = Math.floor(Math.random()*40 + 10);
+            const obstacleInstance = new MovingParts(sizes[2].heigth, sizes[2].width, randomObstacleX, 100, "obstacle", "board", "url('/project1-hedgehog/images/owl.png')");
+            if (this.obstacles.length < 10) {
+                this.obstacles.push(obstacleInstance);
+            }
+        }, 2000)
+    
+        
+        const obstaclesDrop = setInterval(() => {
+            this.obstacles.forEach((obstacle) => {
+                //console.log(obstacle);
+                
+                obstacle.movingDown();
+                if (this.detectCollisionApple(obstacle)) {
+                    clearInterval(obstaclesDrop);
+                    clearInterval(obstaclesAppear);
+                    
+                    console.log("It's game over ...");
+                } else    
+                if (obstacle.positionY <= 0) {
+                    obstacle.domElement.remove();
+                    this.obstacles.shift();
+                    
+                }
+                    
+                
+            })
+        }, 60);
+    }
+    
     shakeTree () {
         const applesDrop = setInterval(() => {
             this.apples.forEach((apple, index) => {
-                //apple.movingDown();
-                
+              
                 if (apple.positionY > apple.stopPosition) {
                     apple.movingDown();
                     
@@ -41,6 +105,9 @@ class Game {
                     this.applesDropped.push(this.apples.splice(index, 1)[0]);
                 }
             })
+            if (this.apples === []) {
+                clearInterval(applesDrop);
+            }
         }, 60);
     }
 
@@ -78,13 +145,14 @@ class Game {
                         //console.log("I'm in the corner ...");
                         this.applesDropped[pickedUp].positionX = 0;
                         this.applesDropped[pickedUp].positionY = pickedUp*this.applesDropped[pickedUp].height;
-                        this.applesDropped[pickedUp].domElement.style.left = this.applesDropped[pickedUp].positionX + "%";
+                        this.applesDropped[pickedUp].domElement.style.left = this.applesDropped[pickedUp].positionX + "vw";
                         this.applesDropped[pickedUp].domElement.style.bottom = this.applesDropped[pickedUp].positionY + "vh";
                         corner = true;
                         pickedUp = undefined;
                         score += 5;
                         document.querySelector("h2 span").innerText = score + " points";
                     } else {
+                        
                         this.hadgehog.pickUp(this.applesDropped[pickedUp], this.hadgehog);
                     }
                     
@@ -129,22 +197,25 @@ class Game {
     }
     detectCollisionTree(playerInstance, target) {
         let tree = document.getElementById(target); 
-            let treeMiddle = ((tree.getBoundingClientRect().right - tree.getBoundingClientRect().left)/2 + 
-            tree.getBoundingClientRect().left)*100/window.innerWidth;
-            if (playerInstance.positionX -2 < treeMiddle && 
-                playerInstance.positionX + 4 > treeMiddle && playerInstance.positionY > 1 && playerInstance.positionY < 10) {
-                    
-                    return true;
-                } else {
-                    return false;
-                }
-            
+        let treeMiddle = ((tree.getBoundingClientRect().right - tree.getBoundingClientRect().left)/2 + 
+        tree.getBoundingClientRect().left)*100/window.innerWidth;
+        //console.log(tree.getBoundingClientRect().right);
+        //console.log(tree.getBoundingClientRect().left);
+        //console.log(treeMiddle);
+
+        if (playerInstance.positionX < treeMiddle && 
+            playerInstance.positionX + playerInstance.width*1.2 > treeMiddle && playerInstance.positionY > 1 && playerInstance.positionY < 10) {
+                
+                return true;
+            } else {
+                return false;
+            } 
     }
     detectCollisionApple(playerInstance) {
         if (this.hadgehog.positionX < playerInstance.positionX + playerInstance.width &&
-            this.hadgehog.positionX + this.hadgehog.width > playerInstance.positionX &&
+            this.hadgehog.positionX + this.hadgehog.width*0.8 > playerInstance.positionX &&
             this.hadgehog.positionY < playerInstance.positionY + playerInstance.height &&
-            this.hadgehog.height + this.hadgehog.positionY > playerInstance.positionY) {
+            this.hadgehog.height*0.8 + this.hadgehog.positionY > playerInstance.positionY) {
             //console.log("I'm in the collision ...");
             return true;
         } else {
@@ -155,7 +226,7 @@ class Game {
 }
 
 class MovingParts {
-    constructor (height, width, positionX, positionY, idByClass, idParentContainer){
+    constructor (height, width, positionX, positionY, idByClass, idParentContainer, backgroundImage){
         this.height = height;
         this.width = width;
         this.positionX = positionX;
@@ -163,6 +234,8 @@ class MovingParts {
         this.domElement = null;
         this.idByClass = idByClass;
         this.idParentContainer = idParentContainer;
+        this.backgroundImage = backgroundImage;
+
         this.createDomElement();
     }
 
@@ -171,17 +244,19 @@ class MovingParts {
         this.domElement = document.createElement('div');
         // set id and css
         
-        if (window.innerWidth >= window.innerHeight) {
-            this.domElement.style.width = window.innerHeight * this.height / 100 + "px";
-            this.domElement.style.height = this.height + "vh";
-        } else {
-            this.domElement.style.width = this.width + "vw";
-            this.domElement.style.height = window.innerWidth * this.width / 100 + "px";
-        }
+        //if (window.innerWidth >= window.innerHeight) {
+        //    this.domElement.style.width = this.height + "vh";
+        //    this.domElement.style.height = this.height + "vh";
+        //} else {
+        //    this.domElement.style.width = this.width + "vw";
+        //    this.domElement.style.height = this.width + "vw";
+        //}
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
         this.domElement.className = this.idByClass;
         this.domElement.style.bottom = this.positionY + "vh";
-        this.domElement.style.left = this.positionX + "%";
-        //this.domElement.style.backgroundImage = "url('/oop-game-codealong/images/player-left.png')"
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.backgroundImage = this.backgroundImage;//"url('/oop-game-codealong/images/player-left.png')"
         // append to the dom
         const boardElm = document.getElementById(this.idParentContainer);
         boardElm.appendChild(this.domElement)
@@ -190,7 +265,7 @@ class MovingParts {
     movingLeft() {
         if (this.positionX > 0) {
             this.positionX--;
-            this.domElement.style.left = this.positionX + "%";
+            this.domElement.style.left = this.positionX + "vw";
         } else {
             this.positionX = 0;
         }
@@ -199,7 +274,7 @@ class MovingParts {
     movingRight() {
         if (this.positionX < 95) {
             this.positionX++;
-            this.domElement.style.left = this.positionX + "%";
+            this.domElement.style.left = this.positionX + "vw";
     } else {
         this.positionX = 100;
     }
@@ -225,14 +300,14 @@ class MovingParts {
 }
 
 class Hadgehog extends MovingParts {
-    constructor (height, width, positionX, positionY, idByClass, idParentContainer){
-        super(height, width, positionX, positionY, idByClass, idParentContainer);
+    constructor (height, width, positionX, positionY, idByClass, idParentContainer, backgroundImage){
+        super(height, width, positionX, positionY, idByClass, idParentContainer, backgroundImage);
       
     }
     pickUp (pickedUpApple, hadgehogDriver) {
         pickedUpApple.positionX = hadgehogDriver.positionX;
         pickedUpApple.positionY = hadgehogDriver.positionY;
-        pickedUpApple.domElement.style.left = pickedUpApple.positionX + "%";
+        pickedUpApple.domElement.style.left = pickedUpApple.positionX + "vw";
         pickedUpApple.domElement.style.bottom = pickedUpApple.positionY + "vh";
         
         //console.log("Im picked up ...");
@@ -242,8 +317,8 @@ class Hadgehog extends MovingParts {
 }
 
 class Apple extends MovingParts {
-    constructor (height, width, positionX, positionY, idByClass, idParentContainer){
-        super(height, width, positionX, positionY, idByClass, idParentContainer);
+    constructor (height, width, positionX, positionY, idByClass, idParentContainer, backgroundImage){
+        super(height, width, positionX, positionY, idByClass, idParentContainer, backgroundImage);
         this.stopPosition = Math.floor(Math.random()*30);
     }
     
